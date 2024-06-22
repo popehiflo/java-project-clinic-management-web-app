8338e29a45c9b1b943001182677b6a3c44b0065d
Pool Petter Hijuela Florian <poolpetterhijuel@hotmail.com>
2024-06-22 10:45:25 -0500

    feat: initialize project (backend) with spring initialzr

---
+++ src/main/java/io/github/popehiflo/clinica/SpringBoot3VanillajsClinicaWebAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.popehiflo.clinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3VanillajsClinicaWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3VanillajsClinicaWebAppApplication.class, args);
+	}
+
+}
