0a3a31a45d7f102958779ebf1bc4745e4611c0a8
Pool Petter Hijuela Florian <poolpetterhijuel@hotmail.com>
2024-06-22 14:34:23 -0500

    feat: add Paciente and Direccion entities, Paciente Repository

---
+++ src/main/java/io/github/popehiflo/clinica/entity/Paciente.java
@@ -0,0 +1,61 @@
+package io.github.popehiflo.clinica.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pacientes")
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String nombre;
+    @Column
+    private String apellido;
+    @Column
+    private String cedula;
+    @Column
+    private LocalDate fechaIngreso;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "domicilio_id", referencedColumnName = "id")
+    private Domicilio domicilio;
+    @Column(unique = true,nullable = false)
+    private String correo;
+
+    public Paciente() {
+    }
+
+    public Paciente(String nombre, String apellido, String cedula, LocalDate fechaIngreso, Domicilio domicilio, String correo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+        this.fechaIngreso = fechaIngreso;
+        this.domicilio = domicilio;
+        this.correo = correo;
+    }
+
+    public Paciente(Long id, String nombre, String apellido, String cedula, LocalDate fechaIngreso, Domicilio domicilio, String correo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+        this.fechaIngreso = fechaIngreso;
+        this.domicilio = domicilio;
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", cedula='" + cedula + '\'' +
+                ", fechaIngreso=" + fechaIngreso +
+                ", domicilio=" + domicilio +
+                ", correo='" + correo + '\'' +
+                '}';
+    }
+}
